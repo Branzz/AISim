2e4157fc0092abc938de5700ba163ca6c16d007d
Brandon Boyer <branboyer@gmail.com>
2021-11-28 03:09:05 -0600

    Add project files.

---
+++ GLFWProject/Main.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include <windows.h>
+
+
+
+#include"shaderClass.h"
+#include"VAO.h"
+#include"VBO.h"
+#include"EBO.h"
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
+GLfloat vertices[] =
+{ //               COORDINATES                  /     COLORS
+	-0.3f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.9f, 0.1f, 0.1f,		// Lower left corner
+	 0.5f, -0.8f * float(sqrt(3)) * 1 / 3, 0.0f,     0.1f, 0.9f, 0.1f,		// Lower right corner
+	 0.1f,	0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     0.1f, 0.1f, 0.9f,		// Upper corner
+	-0.25f,	0.8f * float(sqrt(3)) * 1 / 6, 0.0f,     0.1f, 0.9f, 0.1f,		// Inner left
+	 0.2f,	0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.1f, 0.1f,		// Inner right
+	 0.1f, -0.8f * float(sqrt(3)) * 1 / 3, 0.0f,     0.1f, 0.1f, 0.9f		// Inner down
+};
+GLuint indices[] = {
+	0, 3, 5,
+	3, 2, 4,
+	5, 4, 1
+};
+
+int main() {
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(400, 400, "Test", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Could not create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+	glViewport(0, 0, 400, 400);
+
+	Shader shaderProgram("default.vert", "default.frag");
+
+	VAO VAO1;
+	VAO1.Bind();
+
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
+
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	//glVertex2i(5, 5);
+
+	while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.3f, 0.3f, 0.3f);
+        glClear(GL_COLOR_BUFFER_BIT);
+		shaderProgram.Activate();
+		glUniform1f(uniID, 0.5f);
+		VAO1.Bind();
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glfwSwapBuffers(window);
+        glfwPollEvents();
+	}
+
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
